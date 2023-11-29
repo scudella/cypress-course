4814926d53266d141faf06536e53c4c7a276f17b
Eduardo S. Libardi <eslibardi@gmail.com>
2023-11-29 12:20:29 -0300

    complete basic

--- 02-basics/cypress/e2e/tasks.cy.js
+++ 02-basics/cypress/e2e/tasks.cy.js
@@ -33,4 +33,38 @@ describe('tasks management', () => {
     cy.get('.modal').contains('Add Task').click();
     cy.contains('Please provide values');
   });
+
+  it('should filter tasks', () => {
+    cy.visit('http://localhost:5173');
+    cy.contains('Add Task').click();
+    cy.get('#title').type('My new task');
+    cy.get('#summary').type('My new task summary');
+    cy.get('#category').select('urgent');
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task').should('have.length', 1);
+    cy.get('#filter').select('moderate');
+    cy.get('.task').should('have.length', 0);
+    cy.get('#filter').select('urgent');
+    cy.get('.task').should('have.length', 1);
+    cy.get('#filter').select('all');
+    cy.get('.task').should('have.length', 1);
+  });
+
+  it('should add multiple tasks', () => {
+    cy.visit('http://localhost:5173');
+    cy.contains('Add Task').click();
+    cy.get('#title').type('Task 1');
+    cy.get('#summary').type('First task');
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task').should('have.length', 1);
+
+    cy.contains('Add Task').click();
+    cy.get('#title').type('Task 2');
+    cy.get('#summary').type('Second task');
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task').should('have.length', 2);
+
+    cy.get('.task').eq(0).contains('First task'); // first()
+    cy.get('.task').eq(1).contains('Second task'); // last()
+  });
 });
